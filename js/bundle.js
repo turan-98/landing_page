ae1fd30702abbc765feb66ee8cf007aec3d4d302
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2020-03-10 16:10:04 -0300

    Delete bundle.js

--- js/bundle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var $toggle = document.querySelector(".toggle"),
-    $nav = document.querySelector(".menu");
-$toggle.addEventListener("click", function () {
-  $toggle.classList.toggle("-active");
-  $nav.classList.toggle("-hidden");
-});
